2b047fe30782ff004e1c26fbf07f32ffd0f5c72b
op <op@dchan.network>
2022-09-07 23:03:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+dchan.network relay
+---
+For usage with [dchan-subgraph](https://github.com/dchan-network/dchan-subgraph).
+
+A simple contract to relay JSON messages to the blockchain.
---
+++ contracts/Relay.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Relay {
+    event Message(address from, string jsonMessage);
+
+    function message(string memory jsonMessage) public {
+        emit Message(msg.sender, jsonMessage);
+    }
+}
